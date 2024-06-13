97f463dadae22d6ed4860c65539434141b54fe76
Valentín Fernández Pizzella <74027560+ValFernandez2@users.noreply.github.com>
2024-06-13 12:27:33 -0300

    dadas vuelta las cláusulas

--- avatarProlog.pl
+++ avatarProlog.pl
@@ -1,33 +1,28 @@
-/*elemento(tierra).
-elemento(agua).
-elemento(fuego).
-elemento(aire).*/
-
 %PUNTO 1
 esMaestro(Persona):- esMaestroDe(agua,Persona).
 esMaestro(Persona):- esMaestroDe(tierra,Persona).
 esMaestro(Persona):- esMaestroDe(fuego,Persona).
 esMaestro(Persona):- esMaestroDe(aire,Persona).
 
-esMaestroDe(tierra,toph).
-esMaestroDe(agua,katara).
-esMaestroDe(fuego,zuko).
-esMaestroDe(agua,aang).
-esMaestroDe(fuego,aang).
-esMaestroDe(tierra,aang).
-esMaestroDe(tierra,matias).
-esMaestroDe(agua,valentin).
-esMaestroDe(agua,alf).
-esMaestroDe(tierra,alf).
-esMaestroDe(fuego,lucas).
-esMaestroDe(agua,lucas).
-esMaestroDe(agua,diana).
-esMaestroDe(tierra,diana).
+esMaestroDe(toph,tierra).
+esMaestroDe(katara,agua).
+esMaestroDe(zuko,fuego).
+esMaestroDe(aang,agua).
+esMaestroDe(aang,fuego).
+esMaestroDe(aang,tierra).
+esMaestroDe(matias,tierra).
+esMaestroDe(valentin,agua).
+esMaestroDe(alf,agua).
+esMaestroDe(alf,tierra).
+esMaestroDe(lucas,fuego).
+esMaestroDe(lucas,agua).
+esMaestroDe(diana,agua).
+esMaestroDe(diana,tierra).
 
 
 %PUNTO 2
-esMaestroDe(aire,Persona) :- creoMovimientoEspiritual(Persona).
-esMaestroDe(aire,Persona) :- tieneConexionEspiritual(Persona).
+esMaestroDe(Persona,aire) :- creoMovimientoEspiritual(Persona).
+esMaestroDe(Persona,aire) :- tieneConexionEspiritual(Persona).
 
 creoMovimientoEspiritual(zaheer).
 creoMovimientoEspiritual(aang).
@@ -41,84 +36,86 @@ tieneConexionEspiritual(marquitos).
 %PUNTO 3
 /*
 
-a) ?- esMaestroDe(aire,_).
+a) ?- esMaestroDe(_,aire).
 true .
 
-b) ?- esMaestroDe(fuego,Persona).
+b) ?- esMaestroDe(Persona,fuego).
 Persona = zuko ;
 Persona = aang ;
 Persona = lucas.
 
-c) ?- esMaestroDe(Elemento,Persona).
-Elemento = tierra,
-Persona = toph ;
-Elemento = agua,
-Persona = katara ;
-Elemento = fuego,
-Persona = zuko ;
-Elemento = agua,
-Persona = aang ;
-Elemento = fuego,
-Persona = aang ;
-Elemento = tierra,
-Persona = aang ;
-Elemento = tierra,
-Persona = matias ;
-Elemento = agua,
-Persona = valentin ;
-Elemento = agua,
-Persona = alf ;
-Elemento = tierra,
-Persona = alf ;
-Elemento = fuego,
-Persona = lucas ;
-Elemento = agua,
-Persona = lucas ;
-Elemento = agua,
-Persona = diana ;
-Elemento = tierra,
-Persona = diana ;
-Elemento = aire,
-Persona = zaheer ;
-Elemento = aire,
-Persona = aang ;
-Elemento = aire,
-Persona = tenzin ;
-Elemento = aire,
-Persona = zaheer ;
-Elemento = aire,
-Persona = bumi ;
-Elemento = aire,
-Persona = kai ;
-Elemento = aire,
-Persona = marquitos.
+c) ?- esMaestroDe(Persona,Elemento).
+Persona = toph,
+Elemento = tierra;
+Persona = katara,
+Elemento = agua;
+Persona = zuko,
+Elemento = fuego;
+Persona = aang,
+Elemento = agua;
+Persona = aang,
+Elemento = fuego;
+Persona = aang,
+Elemento = tierra;
+Persona = matias,
+Elemento = tierra;
+Persona = valentin,
+Elemento = agua;
+Persona = alf,
+Elemento = agua;
+Persona = alf,
+Elemento = tierra;
+Persona = lucas,
+Elemento = fuego;
+Persona = lucas,
+Elemento = agua;
+Persona = diana,
+Elemento = agua;
+Persona = diana,
+Elemento = tierra;
+Persona = zaheer,
+Elemento = aire;
+Persona = aang,
+Elemento = aire;
+Persona = tenzin,
+Elemento = aire; 
+Persona = zaheer,
+Elemento = aire;
+Persona = bumi,
+Elemento = aire;
+Persona = kai,
+Elemento = aire;
+Persona = marquitos,
+Elemento = aire.
 
-d) ?- esMaestroDe(Elemento,aang).
+c) ?- esMaestroDe(Persona, Elemento)
+
+d) ?- esMaestroDe(aang,Elemento).
 Elemento = agua ;
 Elemento = fuego ;
 Elemento = tierra ;
 Elemento = aire ;
 
-e) ?- esMaestroDe(Elemento,sokka).
+e) ?- esMaestroDe(sokka,Elemento).
 false.
 */
 
 %PUNTO 4
 tieneDobleMaestria(Persona):-
-    esMaestroDe(Elemento1,Persona),
-    esMaestroDe(Elemento2,Persona),
+    esMaestroDe(Persona,Elemento1),
+    esMaestroDe(Persona,Elemento2),
     Elemento1\=Elemento2.
 
 %PUNTO 5
-confiaEn(zuko,aang).
-confiaEn(toph,aang).
-confiaEn(sokka,aang).
-confiaEn(appa,aang).
-confiaEn(momo,aang).
-confiaEn(Persona,katara):-
-    confiaEn(Persona,aang),
-    not(esMaestroDe(fuego,Persona)).
+confiaEn(aang,zuko).
+confiaEn(aang,aang,toph).
+confiaEn(aang,sokka).
+confiaEn(aang,appa).
+confiaEn(aang,momo).
+confiaEn(katara,Persona):-
+    confiaEn(aang,Persona),
+    not(esMaestroDe(Persona,fuego)).
 
 %PUNTO 6
-esEnemigoDe(Persona,zhao):-esMaestroDe(fuego,Persona).
-esEnemigoDe(zuko,zhao).
+esEnemigoDe(zhao,Persona):-esMaestroDe(Persona,fuego).
+esEnemigoDe(zhao,zuko).
